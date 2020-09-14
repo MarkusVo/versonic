7b77efd8e601d77d74f2a90a2149e5376150ed73
markusvogl <markusvogl@pop-os.localdomain>
2020-09-15 00:09:50 +0200

    adds setup for webaudio and audioContext (currently commented)

--- versonic/src/components/animated-sphere.jsx
+++ versonic/src/components/animated-sphere.jsx
@@ -2,20 +2,34 @@ import React, { useRef, useState, useEffect } from 'react'
 import { useFrame } from 'react-three-fiber'
 import { object, bool, string } from 'prop-types'
 
-let audio;
+const audioContext = new AudioContext()
+let audio
 
 const AnimatedSphere = ({ props, audioFile, playing, paused }) => {
   const mesh = useRef()
+  let src
+  let analyser
+  let bufferLength
+  let dataArray
 
+  // Fucking shit stateManagment with the WebAudio API is tricky
   useEffect(() => {
     if (audioFile) {
+      //still no idea what should happen here but i guess this is the setup of the web audio api
       audio = new Audio(audioFile)
+      audio.load()/* 
+      src = audioContext.createMediaElementSource(audio)
+      analyser = audioContext.createAnalyser()
+      src.connect(analyser)
+      analyser.connect(audioContext.destination)
+      analyser.fftSize = '512'
+      bufferLength = analyser.frequencyBinCount
+      dataArray = new Uint8Array(bufferLength)
+      analyser.getByteFrequencyData(dataArray) */
     }
   }, [audioFile])
 
   if (audio) {
-    audio.load()
-
     if (playing && !paused) {
       audio.play()
     }
